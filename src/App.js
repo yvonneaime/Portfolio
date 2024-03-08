922e14d39b043eea83fd8516a6b636684e9cbca0
Yvonne Aime <yvonneaime0@gmail.com>
2024-03-08 02:31:36 -0500

    analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import About from './Components/About/aboutpg';
 import Home from './Components/Home/Home';
 import NavBar from './Components/NavBar/NavBar';
@@ -8,19 +10,32 @@ import Gallery from './Components/Gallery/paintings';
 import OnlineGallery from './Components/Online/onlineart';
 import Mockups from './Components/Mockups/mockup';
 
+const TRACKING_ID = "G-WBEZKLM3TV"; 
+ReactGA.initialize(TRACKING_ID);
+
+function PageTracker() {
+  let location = useLocation();
+
+  useEffect(() => {
+    // Log page view with current path
+    ReactGA.pageview(location.pathname + location.search + location.hash);
+  }, [location]);
+}
+
 function App() {
   return (
     <HashRouter>
+      <PageTracker />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<><NavBar></NavBar><Projects /></>} />
-        <Route path="/about" element={<><NavBar></NavBar><About /></>} />
-        <Route path="/gallery" element={<><NavBar></NavBar><Gallery /></>} />
-        <Route path="/onlineart" element={<><NavBar></NavBar><OnlineGallery /></>} />
-        <Route path="/mockups" element={<><NavBar></NavBar><Mockups /></>} />
+        <Route path="/projects" element={<><NavBar /><Projects /></>} />
+        <Route path="/about" element={<><NavBar /><About /></>} />
+        <Route path="/gallery" element={<><NavBar /><Gallery /></>} />
+        <Route path="/onlineart" element={<><NavBar /><OnlineGallery /></>} />
+        <Route path="/mockups" element={<><NavBar /><Mockups /></>} />
       </Routes>
     </HashRouter>
   );
-};
+}
 
 export default App;
